88d68f17d770920d99ca68125c431cfd5d4dd4e9
Guillaume <guitiolord@hotmail.fr>
2019-03-20 14:17:10 +0100

    REST Endpoints finished

--- app.js
+++ app.js
@@ -2,73 +2,95 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
-
 const CONNECTION_URL = "mongodb+srv://test:test@denzel-3i4vy.gcp.mongodb.net/test?retryWrites=true";
 const DATABASE_NAME = "Denzel";
-
+const imdb = require('./src/imdb');
+const DENZEL_IMDB_ID = 'nm0000243';
 var app = Express();
-
+var database, collection;
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
-var database, collection;
 
 app.listen(9292, () => {
-    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        database = client.db(DATABASE_NAME);
-        collection = database.collection("Movies");
-        console.log("Connected to `" + DATABASE_NAME + "`!");
-    });
+	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+		if(error) {
+			throw error;
+		}
+		database = client.db(DATABASE_NAME);
+		collection = database.collection("Movies");
+		console.log("Connected to `" + DATABASE_NAME + "`!");
+	});
 });
-
-
-const imdb = require('./src/imdb');
-const DENZEL_IMDB_ID = 'nm0000243';
 
 
 app.get("/movies/populate", async (request, response) => {
 	const movies = await imdb(DENZEL_IMDB_ID);
-    collection.insert( movies, (error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+	collection.insert( movies, (error, result) => {
+		if(error) {
+			return response.status(500).send(error);
+		}
+		response.send(result);
+	});
+});
+
+
+app.get("/movies/search", (request, response) => {
+	var metascore, limit;
+	if (request.query.metascore != undefined){
+		metascore = request.query.metascore;
+	}
+	else {
+		metascore = 0;
+	}
+	if (request.query.limit != undefined){
+		limit = request.query.limit;
+	}
+	else {
+		limit = 0;
+	}
+	collection.aggregate([{$match: {metascore: {$gte: Number(metascore)}}}, {$sample: {size: Number(limit)}}, {$sort: {"metascore": -1}}]).toArray((error, result) => {
+		if (error) {
+			return response.status(500).send(error);
+		}
+		response.send(result);
+	}); 
 });
 
 
 app.get("/movies", async (request, response) => {
-	var query = { metascore : { $gt: 70 }};
+	var query = {metascore: { $gt: 70 }};
 	var n = await collection.countDocuments(query);
 	var r = await Math.floor(Math.random() * n);
-    collection.find(query).limit(1).skip(r).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+	collection.find(query).limit(1).skip(r).toArray((error, result) => {
+		if(error) {
+			return response.status(500).send(error);
+		}
+		response.send(result);
+	});
 });
 
 
 app.get("/movies/:id", (request, response) => {
-    collection.findOne({ "id": request.params.id }, (error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+	collection.findOne({"id": request.params.id}, (error, result) => {
+		if(error) {
+			return response.status(500).send(error);
+		}
+		response.send(result);
+	});
 });
 
-/*
-app.get("/movies/search", (request, response) => {
-    collection.findOne({ "id": request.params.id }, (error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+
+app.post("/movies/:id", (request, response) => {
+	collection.updateOne({"id": request.params.id}, {$set: {"date": request.body.date, "review": request.body.review}}, (error, result) => {
+		if (error) {
+			return response.status(500).send(error);
+		}
+	});
+	collection.findOne({"id": request.params.id}, (error, result) => {
+		if(error) {
+			return response.status(500).send(error);
+		}
+		response.send(result);
+	});
 });
-*/
--- test.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://test:test@denzel-3i4vy.gcp.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
